c42fd22ffc80d5ce16a77404e570d5ee7905550f
Yandé Guissé <yandeiguiss2@gmail.com>
2026-02-26 23:42:03 +0000

    first commit

---
+++ src/main/java/sn/isi/l3gl/core/task_core/TaskCoreApplication.java
@@ -0,0 +1,13 @@
+package sn.isi.l3gl.core.task_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/isi/l3gl/core/task_core/TaskCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.isi.l3gl.core.task_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
